8cf3491237bebdf5eee6f4f9b2ea74a17e4b1dc2
DucNguyen <ducnguyenpy1221@gmail.com>
2021-09-28 01:30:21 +0700

    change log

--- database/pubsub/publisher/publisher_test.go
+++ database/pubsub/publisher/publisher_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestPublishMessage(t *testing.T) {
 	config := Config{
-		projectID: "thetan-staging",
-		topicID : "BATTLE_LOGS_TOPIC",
+		ProjectID: "thetan-staging",
+		TopicID : "BATTLE_LOGS_TOPIC",
 	}
 	InitConfiguration(&config)
 	message := "I'm handsome"
