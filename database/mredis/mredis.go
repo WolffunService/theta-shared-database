3d727b0f17a3bb10d37902cfc2b881c629e20c88
Fuwn <phuong@wolffungame.com>
2021-10-14 10:45:31 +0700

    move redis object to mredis

--- database/mredis/mredis.go
+++ database/mredis/mredis.go
@@ -1,6 +1,10 @@
 package mredis
 
 import (
+	"context"
+	"encoding/json"
+	"time"
+
 	"github.com/WolffunGame/theta-shared-database/database/mredis/thetanlock"
 	goredislib "github.com/go-redis/redis/v8"
 	"github.com/go-redsync/redsync/v4/redis"
@@ -24,4 +28,32 @@ func ConnectRedis(config *RedisConnectionConfig) {
 
 func GetClient() *goredislib.Client {
 	return client
+}
+
+// Set struct to redis
+// EX used: err = util.SetObject(ctx, "key", userModel, 86400)
+func SetObject(ctx context.Context, key string, value interface{}, expirationSecond int) error {
+	jsonStr, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return client.Set(ctx, key, jsonStr, time.Duration(expirationSecond)*time.Second).Err()
+}
+
+// Get struct from redis
+// EX used: existsFlag, err := util.GetObject(ctx, "key", &userModel)
+func GetObject(ctx context.Context, key string, refObj interface{}) (bool, error) {
+	bytes, err := client.Get(ctx, key).Bytes()
+	if err != nil {
+		return false, err
+	}
+	if err == goredislib.Nil {
+		// Key not exists
+		return false, nil
+	}
+	err = json.Unmarshal(bytes, &refObj)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
