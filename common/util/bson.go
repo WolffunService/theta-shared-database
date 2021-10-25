cce2b3d416bf202c6a3dff208453a7da9e799193
tinhwolffun <minhtinh@wolffungame.com>
2021-10-25 11:37:35 +0700

    add more bson util, remove check nil

--- common/util/bson.go
+++ common/util/bson.go
@@ -2,27 +2,30 @@ package util
 
 import "go.mongodb.org/mongo-driver/bson"
 
+func BsonAdd(b bson.D,key string, value interface{}) bson.D {
+	return append(b, bson.E{Key:key, Value: value})
+}
+
 func BsonIncrease(b bson.D,key string, value int) bson.D {
-	if b == nil {b = bson.D{}}
 	return append(b, bson.E{Key:"$inc", Value: bson.M{key:value}})
 }
 
 func BsonIncrease64(b bson.D,key string, value int64) bson.D {
-	if b == nil {b = bson.D{}}
 	return append(b, bson.E{Key:"$inc", Value: bson.M{key:value}})
 }
 
 func BsonSet(b bson.D,key string, value interface{}) bson.D {
-	if b == nil {b = bson.D{}}
 	return append(b, bson.E{Key:"$set", Value: bson.M{key:value}})
 }
 
+func BsonNotEqual(b bson.D,key string, value interface{}) bson.D {
+	return append(b, bson.E{Key:key, Value: bson.M{"$ne":value}})
+}
+
 func BsonPush(b bson.D,key string, value interface{}) bson.D {
-	if b == nil {b = bson.D{}}
 	return append(b, bson.E{Key:"$push", Value: bson.M{key:value}})
 }
 
 func BsonPushMultiArr(b bson.D,key string, value interface{}) bson.D {
-	if b == nil {b = bson.D{}}
 	return append(b, bson.E{Key:"$push", Value: bson.E{Key:"$each", Value: bson.M{key:value}}})
 }
