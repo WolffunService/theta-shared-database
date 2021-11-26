35cfe874e6e84f7205a4b075a0c6eebc847b2ad9
tinhwolffun <minhtinh@wolffungame.com>
2021-11-26 20:43:42 +0700

    option connect failover - check empty string

--- database/mredis/mredis.go
+++ database/mredis/mredis.go
@@ -18,13 +18,21 @@ func NewPool() redis.Pool {
 }
 
 func ConnectRedisV2(config *RedisConnectionConfig) {
-	client = goredislib.NewFailoverClient(&goredislib.FailoverOptions{
-		MasterName: config.MasterName,
-		SentinelAddrs: config.SentinelAddrs,
-		Username: config.UserName,
-		Password: config.Password,
-		//SentinelPassword: config.SentinelPassword,
-	})
+	ops := &goredislib.FailoverOptions{}
+	if len(config.MasterName)>0{
+		ops.MasterName = config.MasterName
+	}
+	if len(config.Password) > 0{
+		ops.Password = config.Password
+	}
+	if len(config.SentinelAddrs)>0{
+		ops.SentinelAddrs = config.SentinelAddrs
+	}
+	if len(config.SentinelPassword) > 0{
+		ops.SentinelPassword = config.SentinelPassword
+	}
+
+	client = goredislib.NewFailoverClient(ops)
 	client.Ping(context.Background())
 	thetanlock.InitPool(NewPool())
 }
