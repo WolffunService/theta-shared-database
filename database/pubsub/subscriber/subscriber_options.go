3ee551fbb10f5abae034e2543f0ab35b061678bc
Phước Trung <trungdlp@wolffungame.com>
2022-03-22 18:05:27 +0700

    add NotAckFailedMessage options

--- database/pubsub/subscriber/subscriber_options.go
+++ database/pubsub/subscriber/subscriber_options.go
@@ -8,6 +8,12 @@ type SubscriberOption func(*dynamicConfig)
 
 func AckFailedMessage() SubscriberOption {
 	return func(dc *dynamicConfig) {
+		dc.AckSuccessOnly = false
+	}
+}
+
+func NotAckFailedMessage() SubscriberOption {
+	return func(dc *dynamicConfig) {
 		dc.AckSuccessOnly = true
 	}
 }
