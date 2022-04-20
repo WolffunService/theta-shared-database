ba25f535f11b19b9f87f211c11ad0653b3e40dac
ponlv <96333478+ponlv@users.noreply.github.com>
2022-04-20 19:03:37 +0700

    update event handle

---
+++ user/userevent/userevent_handler.go
@@ -0,0 +1,13 @@
+package userevent
+
+var HeroNFTChange = func(userId string, value int) {
+
+}
+
+var BattleTotalChange = func(userId, value int) {
+
+}
+
+var CreatorViewPointChange = func(userId, value int) {
+
+}
---
+++ user/userevent/userevent_register.go
@@ -0,0 +1,7 @@
+package userevent
+
+//func RegisterEventCallback() {
+//	if !eventbus.S.HasCallback(HERONFTCHANGE) {
+//		_ = eventbus.S.Subscribe(EventTopic+"VisitContent_Viewer", ViewerMilestoneProcess)
+//	}
+//}
