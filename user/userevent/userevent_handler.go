40038fb3478a715da12a6c59170979e8257aa33b
ponlv <96333478+ponlv@users.noreply.github.com>
2022-04-20 19:15:41 +0700

    Update userevent_handler.go

--- user/userevent/userevent_handler.go
+++ user/userevent/userevent_handler.go
@@ -11,3 +11,23 @@ var BattleTotalChange = func(userId, value int) {
 var CreatorViewPointChange = func(userId, value int) {
 
 }
+
+var IAPValueChange = func(userId, value int) {
+
+}
+
+var MKPValueChange = func(userId, value int) {
+
+}
+
+var MKPFrequencyChange = func(userId, value int) {
+
+}
+
+var BattleFrequencyChange = func(userId, value int) {
+
+}
+
+var PlayerBattleChange = func(userId, value int) {
+
+}
