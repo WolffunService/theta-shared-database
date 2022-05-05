350e9386963db6531f3356ab67569a00a68cc61e
vhoanglam <93864760+vhoanglam@users.noreply.github.com>
2022-05-05 19:56:03 +0700

    them cac stat khac

--- database/elasticsearch/model/user.go
+++ database/elasticsearch/model/user.go
@@ -1,9 +1,6 @@
 package model
 
 import (
-	"encoding/json"
-	"github.com/elastic/go-elasticsearch/v8/esapi"
-	"strings"
 	"time"
 )
 
@@ -21,12 +18,12 @@ type StatName string
 
 // UserModel Các stat cố định của player sẽ define tại struct này để phục vụ filtering
 type UserModel struct {
-	ID           string    `json:"id"`
-	Mail         string    `json:"mail"`
-	Country      string    `json:"country"`
-	Created      time.Time `json:"created"`
-	Opened       time.Time `json:"opened"`
-	WalletLinked time.Time `json:"wallet_linked"`
+	ID              string    `json:"id"`
+	Mail            string    `json:"mail"`
+	Country         string    `json:"country"`
+	Created         time.Time `json:"created"`
+	GameOpened      time.Time `json:"game_opened"`
+	WalletConnected time.Time `json:"wallet_connected"`
 }
 
 type BattleStatMapping struct {
@@ -42,15 +39,56 @@ type BattleStatMapping struct {
 	Timestamp  time.Time `json:"@timestamp"`
 }
 
+func (BattleStatMapping) Index() string {
+	return "playerstats-battle"
+}
+
+type IAPStatMapping struct {
+	User       UserModel `json:"user"`
+	PackID     string    `json:"pack_id"`
+	Source     string    `json:"source"`
+	Price      int64     `json:"price"`
+	PriceType  string    `json:"price_type"`
+	PowerPoint int       `json:"power_point"`
+	HeroID     string    `json:"hero_id"`
+	SkinID     string    `json:"skin_id"`
+	Timestamp  time.Time `json:"@timestamp"`
+}
+
+func (IAPStatMapping) Index() string {
+	return "playerstats-iap"
+}
+
+type MarketPlaceStatMapping struct {
+	User       UserModel `json:"user"`
+	MarketType string    `json:"market_type"`
+	Action     string    `json:"action"`
+	RefType    string    `json:"ref_type"`
+	TokenID    string    `json:"token_id"`
+	ItemID     string    `json:"item_id"`
+	Price      int       `json:"price"`
+	Currency   string    `json:"currency"`
+	Country    string    `json:"country"`
+
+	HeroTypeID int    `json:"hero_type_id"`
+	SkinRarity string `json:"skin_rarity"`
+	HeroRarity string `json:"hero_rarity"`
+	HeroLevel  string `json:"hero_level"`
+	Trophy     string `json:"trophy"`
+
+	ItemRarity string `json:"item_rarity"`
+	ItemTypeID int    `json:"item_type_id"`
+	ItemType   string `json:"item_type"`
+}
+
+func (MarketPlaceStatMapping) Index() string {
+	return "playerstats-marketplace"
+}
+
 type WalletStatMapping struct {
 	User UserModel `json:"user"`
 	THC  int64     `json:"thc"`
 	THG  int64     `json:"thg"`
-}
-
-type IAPStatMapping struct {
-	User  UserModel `json:"user"`
-	Value int64     `json:"value"`
 }
 
 type UniversalUserStatMapping struct {
@@ -58,20 +96,4 @@ type UniversalUserStatMapping struct {
 	StatName  string      `json:"stat_name"`
 	StatValue float64     `json:"stat_value"`
 	Timestamp time.Time   `json:"@timestamp"`
-}
-
-func (u UniversalUserStatMapping) String() string {
-	res, _ := json.Marshal(u)
-	return string(res)
-}
-
-func (u UniversalUserStatMapping) GetIndexRequest() esapi.IndexRequest {
-	return esapi.IndexRequest{
-		Index: "user-stat",
-		Body:  strings.NewReader(u.String()),
-	}
-}
-
-func (u BattleStatMapping) Index() string {
-	return "playerstats-battle"
 }
